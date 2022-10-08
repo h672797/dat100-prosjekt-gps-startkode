856fa3d086c26bdd04eb88717a57fb2da300fde7
h150697 <siri_norstrand@hotmail.com>
2022-10-08 16:36:57 +0200

    Update GPSUtils.java
    3a)

--- src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
+++ src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
@@ -23,12 +23,20 @@ public class GPSUtils {
 	}
 
 	public static double findMin(double[] da) {
-
+		// TODO - START
 		double min;
 
-		// TODO - START
-
-		throw new UnsupportedOperationException(TODO.method());
+		min = da[0];
+		
+		for (double d: da) {
+			if(d < min) {
+				min = d;
+			}
+		}
+		
+		return min;
+		
+		//throw new UnsupportedOperationException(TODO.method());
 
 		// TODO - SLUT
 
